53150fdc34a7573837f7b6a0930a70a3d754dcd1
yumingzhe <yumingzhe@live.cn>
2017-11-05 21:46:37 +0800

    增加part02部分实现

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -12,30 +12,30 @@ public class Part02Mono {
 
 //========================================================================================
 
-	// TODO Return an empty Mono
-	Mono<String> emptyMono() {
-		return null;
-	}
+    // TODO Return an empty Mono
+    Mono<String> emptyMono() {
+        return Mono.empty();
+    }
 
 //========================================================================================
 
-	// TODO Return a Mono that never emits any signal
-	Mono<String> monoWithNoSignal() {
-		return null;
-	}
+    // TODO Return a Mono that never emits any signal
+    Mono<String> monoWithNoSignal() {
+        return null;
+    }
 
 //========================================================================================
 
-	// TODO Return a Mono that contains a "foo" value
-	Mono<String> fooMono() {
-		return null;
-	}
+    // TODO Return a Mono that contains a "foo" value
+    Mono<String> fooMono() {
+        return null;
+    }
 
 //========================================================================================
 
-	// TODO Create a Mono that emits an IllegalStateException
-	Mono<String> errorMono() {
-		return null;
-	}
+    // TODO Create a Mono that emits an IllegalStateException
+    Mono<String> errorMono() {
+        return null;
+    }
 
 }
