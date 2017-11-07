a92b0a6510d0fd2bc0c3e0b09b454ab6fb15f1e9
yumingzhe <yumingzhe@live.cn>
2017-11-07 11:08:06 +0800

    修复part01实现

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -4,8 +4,6 @@ import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.LongStream;
 
 /**
  * Learn how to create Flux instances.
@@ -15,38 +13,23 @@ import java.util.stream.LongStream;
  */
 public class Part01Flux {
 
-//========================================================================================
-
-    // TODO Return an empty Flux
     Flux<String> emptyFlux() {
         return Flux.empty();
     }
 
-//========================================================================================
-
-    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
     Flux<String> fooBarFluxFromValues() {
         return Flux.just("foo", "bar");
     }
 
-//========================================================================================
-
-    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
     Flux<String> fooBarFluxFromList() {
         return Flux.fromIterable(Arrays.asList("foo", "bar"));
     }
 
-//========================================================================================
-
-    // TODO Create a Flux that emits an IllegalStateException
     Flux<String> errorFlux() {
         return Flux.error(new IllegalStateException());
     }
 
-//========================================================================================
-
-    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
     Flux<Long> counter() {
-        return Flux.range(0, 9).interval(Duration.ofMillis(100));
+        return Flux.interval(Duration.ofMillis(100)).take(10);
     }
 }
